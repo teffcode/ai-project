94501303096a53f60c7b798d2acb271b5f3dccad
teffcode <tefy.1@live.com>
2025-03-10 19:40:02 -0500

    feature(upload): remove log viewer component

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -9,7 +9,6 @@ import SectionHeader from "@/components/UI/SectionHeader";
 import BodySection from "@/components/UI/BodySection";
 import MainSection from "@/components/UI/MainSection";
 import Notification from "@/components/UI/Notification";
-import LogViewer from "@/components/logs/LogViewer";
 import { useS3ImageUpload } from "@/hooks/useS3ImageUpload";
 
 export default function Upload() {
